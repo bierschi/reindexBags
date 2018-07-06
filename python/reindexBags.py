d73ce95f4af7b422ae7628205914762984d17bbc
Christian Bierschneider <christian.bierschneider@web.de>
2018-07-06 23:36:28 +0200

    Added final python script reindexBags.py

--- python/reindexBags.py
+++ python/reindexBags.py
@@ -2,39 +2,41 @@
 import argparse
 import os
 import subprocess
+import sys
 
 
-def reindex_rosbags(source_folder):
+def reindexBags(source_folder):
 
     active_files = [f for f in os.listdir(source_folder) if f.endswith('active')]
-    active_files_concat = ' '.join(active_files)
-    command = "rosbag reindex " + active_files_concat
+    if len(active_files) > 0:
+        print("start reindexing...")
+        command = "rosbag reindex " + ' '.join(active_files)
 
-    reindex_proc = subprocess.Popen(command, stdin=subprocess.PIPE, shell=True, cwd=source_folder,
-                                     executable='/bin/bash')
-    output, error = reindex_proc.communicate()
-    reindex_proc.wait()
+        reindex_proc = subprocess.Popen(command, stdin=subprocess.PIPE, shell=True, cwd=source_folder,
+                                         executable='/bin/bash')
+        output, error = reindex_proc.communicate()
+        if error:
+            print("error occured while running 'rosbag reindex'")
+            sys.exit(0)
+        reindex_proc.wait()
 
-    os.chdir(source_folder)
-    orig_files = [f for f in os.listdir(source_folder) if 'orig' in f]
-    for file in orig_files:
-        os.remove(file)
+        os.chdir(source_folder)
+        [os.remove(f) for f in os.listdir(source_folder) if 'orig' in f]
+        [os.rename(f, f[:-7]) for f in os.listdir(source_folder) if f.endswith('active')]
+        print("finished reindexing")
 
-    reindexed_files = [os.rename(f, f[:-7]) for f in os.listdir(source_folder) if f.endswith('active')]
-    reindexed_files_concat = ' '.join(reindexed_files)
-
-    print("Following files successfully reindexed: {}".format(reindexed_files_concat))
+    else:
+        print("no active bag files in given source folder!")
 
 
 def main():
 
     parser = argparse.ArgumentParser(description='command line tool ro reindex rosbag files')
     parser.add_argument('-s', '--source', action='store', dest='source_folder', help='specify the source folder', required=True)
-    #parser.add_argument('-t', '--target', action='store', dest='target_folder', help='specify the target folder', required=True)
     
     args = parser.parse_args()
 
-    reindex_rosbags(source_folder=args.source_folder)
+    reindexBags(source_folder=args.source_folder)
 
 
 if __name__ == '__main__':
